6fd50cce796e60df77696f6090ee3f2dd43e4adc
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2020-07-04 16:20:57 +0900

    fix statue

--- index.js
+++ index.js
@@ -3,42 +3,26 @@ const github = require("@actions/github");
 
 async function run() {
   try {
-    const jobStatusSuccess = 'Success'
-    const jobStatusFailure = 'Failure'
-    const jobStatusPending = 'Pending'
-
-    const deployStateSuccess = 'success'
-    const deployStateFailure = 'failure'
-    const deployStatePending = 'pending'
-
     const context = github.context;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
     const deploymentId = core.getInput('deployment-id', {required: false}) || context.payload.deployment.id;
     const token = core.getInput('token', {required: true});
-    const jobStatus = core.getInput('status', {required: true});
+    const state = core.getInput('state', {required: true});
     const url = core.getInput('log-url', {required: false}) || defaultUrl;
     const description = core.getInput('description', {required: false});
     const env = core.getInput('environment', {required: false});
     const envUrl = core.getInput('environment-url', {required: false});
 
-    let deployState = deployStatePending 
-    if (jobStatus == jobStatusSuccess) {
-      deployState = deployStateSuccess
-    } else if  (jobStatus == jobStatusFailure) {
-      deployState = deployStateFailure
-    }
-
     const client = new github.GitHub(token);
     const params = {
       ...context.repo,
       deployment_id: deploymentId,
-      state: deployState,
+      state,
       log_url: url,
       target_url: url,
       description,
     };
-
     console.log(params)
 
     if (env) {
